e191336aa1a4cb6038bb13997eea9655e417cb22
will <willamssouza917@gmail.com>
2023-02-20 22:02:13 -0300

    inicializando a engine do jogo

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var engine = {
+    "cores": ['green', 'purple', 'pink', 'red', 'yellow', 'orange', 'grey'],
+
+    "hexadecimais": {
+        "green": "#02ef00",
+        "purple": "#790093",
+        "red": "#e90808",
+        "yellow": "#e7d703",
+        "black": "#141414",
+        "orange": "#f16529",
+        "grey": "#808080"
+    },
+
+    "moedas":0
+}
